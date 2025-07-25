0b99761457b221b0d7c331136ea805c53ac965a5
testomkar43 <omkarshintre56621@gmail.com>
2025-07-25 16:46:28 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './components/Register';
-import Login from './components/Login';
+import Login from './components/Logintemp';
 import Dashboard from './components/Dashboard';
 
 function App() {
